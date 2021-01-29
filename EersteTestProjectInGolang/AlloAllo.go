01a14fb17fc4e9d5442222546d5f6047da64fed2
thomtech <42716416+thomtech@users.noreply.github.com>
2021-01-29 09:14:53 +0100

    Update AlloAllo.go

--- EersteTestProjectInGolang/AlloAllo.go
+++ EersteTestProjectInGolang/AlloAllo.go
@@ -1,6 +1,20 @@
 package main
 
+import "fmt"
+
+var thom string = "Jij snapt nu een string"
+
+thom  +=  "..."
+
 func main() {
 
-	println("hoooii")
+	fmt.Println("lalallaalla\n" +
+		"asdfasdf\n" +
+		"df")
+
+	x := "Thomboooooo"
+	fmt.Println(x)
+ md
+	fmt.Println(thom)
+
 }
