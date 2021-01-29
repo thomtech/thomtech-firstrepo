b82f8b081d7968ac1ed4ade945a9fd1579b2186b
thomtech <42716416+thomtech@users.noreply.github.com>
2021-01-29 09:17:40 +0100

    Update Halloootjes.ps1

--- Halloootjes.ps1
+++ Halloootjes.ps1
@@ -1,3 +1,8 @@
 ﻿$tekst = "Lorem Ipsum bladie bla"
 
 Write-Host $tekst -ForegroundColor Green
+Write-Host $tekst -ForegroundColor Black
+Write-Host $tekst -ForegroundColor Blue
+Write-Host $tekst -ForegroundColor Red
+Write-Host $tekst -ForegroundColor DarkCyan
+Write-Host $tekst -ForegroundColor DarkGray
